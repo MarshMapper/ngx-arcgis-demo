6655e03d735d04a7712e228e526f9b8bda6c5aeb
Paul <paul@goodhops.com>
2024-06-09 09:00:21 -0400

    Show progress indicator when calling 3rd party APIs for tide, bird observations or weather.

--- src/app/services/progress.service.ts
+++ src/app/services/progress.service.ts
@@ -1,17 +1,32 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable } from 'rxjs';
 
-// simple shared service to track when work is in progress so that feedback can be provided to the user
+// simple shared service to track when work is in progress so that feedback can be provided to the user.
+// allows for multiple components to set the work in progress state and will track when the last is compoleted.
 @Injectable({
   providedIn: 'root'
 })
 export class ProgressService {
   public workInProgress: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(false);
+  private inProgressCount: number = 0;
+
   constructor() { }
   getWorkInProgress(): Observable<boolean> {
     return this.workInProgress.asObservable();
   }
   setWorkInProgress(value: boolean): void {
-    this.workInProgress.next(value);
+    if (value) {
+      if (this.inProgressCount === 0) {
+        this.workInProgress.next(true);
+      }
+      this.inProgressCount++;
+    } else {
+      if (this.inProgressCount === 0) {
+        return;
+      }
+      if (! --this.inProgressCount) {
+        this.workInProgress.next(false);
+      }
+    }
   }
 }
--- src/app/services/weather.service.ts
+++ src/app/services/weather.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import esriRequest from '@arcgis/core/request';
+import { ProgressService } from './progress.service';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ export class WeatherService {
   pointsBaseUrl: string = 'https://api.weather.gov/points/';
   weatherActionId: string = "weather-here";
 
-  constructor() { }
+  constructor(private progressService: ProgressService) { }
   getWeatherAction(): any {
     return {
       title: "Weather",
@@ -19,15 +20,28 @@ export class WeatherService {
   getWeatherActionId(): string {
     return this.weatherActionId;
   }
+  // get the weather predictions for a given location.  it's a 2 step process.  first get the forecast url
+  // from the location, then get the forecast from the url returned
   getWeatherPredictions(popup: any): Promise<any> {
     let url: string = `${this.pointsBaseUrl}${popup.location.latitude},${popup.location.longitude}`;
+    this.progressService.setWorkInProgress(true);
+
     return esriRequest(url)
       .then((response: any) => {
+
         let properties = response.data.properties;
+        // get the actual forecast using the url returned
         return esriRequest(properties.forecast)
           .then((response: any) => {
+            this.progressService.setWorkInProgress(false);
             return response.data.properties;
+          })
+          .catch((error: any) => {
+            this.progressService.setWorkInProgress(false);
           });
+      })
+      .catch((error: any) => {
+        this.progressService.setWorkInProgress(false);
       });
   }
 }
